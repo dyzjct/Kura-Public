34ca36f64e1123866d901aa71d2fbff54e2a2646
chunfeng666 <chunfeng65777@gmail.com>
2022-12-11 14:16:38 +0800

    更新 Welcomer.kt

--- src/main/java/me/dyzjct/kura/module/hud/huds/Welcomer.kt
+++ src/main/java/me/dyzjct/kura/module/hud/huds/Welcomer.kt
@@ -3,6 +3,7 @@ package me.dyzjct.kura.module.hud.huds
 import me.dyzjct.kura.manager.GuiManager
 import me.dyzjct.kura.module.Category
 import me.dyzjct.kura.module.HUDModule
+import me.dyzjct.kura.utils.color.ColorUtil
 import me.dyzjct.kura.utils.font.CFont
 import me.dyzjct.kura.utils.font.RFontRenderer
 import net.minecraft.client.Minecraft
@@ -13,21 +14,35 @@ import java.util.concurrent.CopyOnWriteArrayList
  */
 @HUDModule.Info(name = "Welcomer", x = 160, y = 160, width = 100, height = 10, category = Category.HUD)
 class Welcomer : HUDModule() {
+    private var mode = msetting("Mode", Mode.Rainbow)
+    private var color = csetting("Color", Color(210, 100, 165)).m(mode, Mode.Custom)
     var fonts = CopyOnWriteArrayList<RFontRenderer>()
-
     var font = RFontRenderer(CFont.CustomFont("/assets/fonts/font.ttf", 47.0f, 0), true, false)
     override fun onRender() {
         if (!fonts.contains(font)) {
             fonts.add(font)
         }
-        val fontColor = Color(
-            GuiManager.getINSTANCE().red / 255f,
-            GuiManager.getINSTANCE().green / 255f,
-            GuiManager.getINSTANCE().blue / 255f,
-            1f
-        ).rgb
+
+        fun fontColor(): Int {
+            val fontColor = Color(
+                GuiManager.getINSTANCE().red / 255f,
+                GuiManager.getINSTANCE().green / 255f,
+                GuiManager.getINSTANCE().blue / 255f,
+                1f
+            ).rgb
+            val custom = Color(color.value.red, color.value.green, color.value.blue).rgb
+            when (mode.value) {
+                Mode.Rainbow -> return ColorUtil.staticRainbow().rgb
+                Mode.GuiSync -> return fontColor
+                Mode.Custom -> return custom
+            }
+            return -1
+        }
         val Final = "Hello " + Minecraft.getMinecraft().player.name + "! Thanks for use:)"
-        fontRenderer.drawString(Final, x + 2, y + 4, fontColor)
+        fontRenderer.drawString(Final, x + 2, y + 4, fontColor())
         width = fontRenderer.getStringWidth(Final) + 4
+    }
+    enum class Mode {
+        Rainbow, GuiSync, Custom
     }
 }
